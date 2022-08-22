5c514348fa6ca407bc6a947ebc0b498caa877e94
Ahmed Taha <xahmedtaha930@gmail.com>
2022-08-22 21:02:49 +0200

    Bug Fixes

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['*'],
+    'paths' => ['*', 'api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_headers' => ['*'],
--- routes/userApi.php
+++ routes/userApi.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['middleware'=> ['app_version']],function (){
+Route::group(['middleware'=> ['app_version', 'cors']],function (){
     //tables
     Route::get('/testfirestore','OrderController@testFirestore');
     Route::get('/getToken','GuestController@getToken');
