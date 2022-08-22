ac9ed07153a11b279fe769a346e7388345ae11ed
Ahmed Taha <xahmedtaha930@gmail.com>
2022-08-22 21:40:41 +0200

    Remove App Based CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,8 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        // header('Access-Control-Allow-Origin: *');
+        // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         return $next($request);
     }
 }
